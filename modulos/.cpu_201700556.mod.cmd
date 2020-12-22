bd35fddb33accaefa0cfa0c3a2feeab30a18b27e
juarezbarneond44 <69942635+juarezbarneond44@users.noreply.github.com>
2020-12-22 09:05:08 -0600

    Delete .cpu_201700556.mod.cmd

--- modulos/.cpu_201700556.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/juan/Escritorio/Proyecto1/modulos/cpu_201700556.mod := { echo  /home/juan/Escritorio/Proyecto1/modulos/cpu_201700556.o; echo; } > /home/juan/Escritorio/Proyecto1/modulos/cpu_201700556.mod
